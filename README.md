de0f59b94773265817dfc3d873e8edf9c7fae420
YashRg-12 <yrg95019@gmail.com>
2022-07-10 13:35:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Keeper-App
+A note making React App
+
+Can be viewed at https://cosmic-kitsune-a29b92.netlify.app/
---
+++ build/precache-manifest.8a7abe87341e9c08be33aefab2f9537b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd12f0c1b98e1b368e0ffa6f4d231734",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91dcddfb80aa5ef12447",
+    "url": "/static/js/2.32b9d141.chunk.js"
+  },
+  {
+    "revision": "0096a0a8d5596b1d1311",
+    "url": "/static/js/main.7b5ecb46.chunk.js"
+  },
+  {
+    "revision": "921d0da837860e5eca9c",
+    "url": "/static/js/runtime-main.0ce15bf1.js"
+  }
+]);
